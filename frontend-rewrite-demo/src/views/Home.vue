eec4b38643497486aa5a54120a3533858bf80e39
Michael Woodruff <michaelwoodruff@mwc-021001.dhcp.missouri.edu>
2019-10-17 15:31:31 -0500

    store contributors to component state in Home.vue

--- frontend-rewrite-demo/src/views/Home.vue
+++ frontend-rewrite-demo/src/views/Home.vue
@@ -8,13 +8,20 @@
 <script>
 export default {
     name: 'Home', 
+    data: {
+        contributors: []
+    }, 
     methods: {
         getContributors() {
             console.log('getting contributors');
             let repo = this.$store.state.selectedRepoOne;
             fetch(`http://augur.osshealth.io:5000/api/unstable/repo-groups/${repo.groupId}/repos/${repo.repoId}/contributors`)
                 .then(res => res.json())
-                .then(res => console.log(res));
+                .then(res => {
+                    res.forEach(contributor => {
+                        this.contributors.push(contributor);
+                    });
+                });
         }
     }
 }
