472e8efc981f00fc118bc9274b609224d5f54b42
Weiyu Feng <47650296+0WeiyuFeng0@users.noreply.github.com>
2019-11-21 15:51:32 -0600

    deploy the project to the firebase server

--- frontend/final-project-app/src/environments/environment.ts
+++ frontend/final-project-app/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBXDbXdkn230yWmcO_DWg9gySQS3rpw4mc",
+    authDomain: "group12sprint3.firebaseapp.com",
+    databaseURL: "https://group12sprint3.firebaseio.com",
+    projectId: "group12sprint3",
+    storageBucket: "group12sprint3.appspot.com",
+    messagingSenderId: "475768359444",
+    appId: "1:475768359444:web:bbbb32d765b15be4827501",
+    measurementId: "G-XT3NF0M29Z"
+  }
 };
 
 /*
